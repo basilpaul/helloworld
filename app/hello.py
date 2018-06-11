bb79547384354a7adc44d2bed287d85b91494bc1
Basil Paul <basil.paul@digitaslbi.com>
2018-06-11 18:41:08 +0100

    adding healthcheck

--- app/hello.py
+++ app/hello.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from healthcheck import HealthCheck
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
         return 'Hello, World!'
-        
+
+health = HealthCheck(app, "/healthcheck")
+
+def chef_app_health():
+	return True, "Health OK"
+
+health.add_check(chef_app_health)
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
