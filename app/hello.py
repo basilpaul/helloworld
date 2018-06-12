751a21c2999c4612bd4af994ae3cd812738ea91a
Basil Paul <basil.paul@digitaslbi.com>
2018-06-12 11:57:32 +0100

    change method names

--- app/hello.py
+++ app/hello.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
         return 'Hello, World!'
-
+        
 health = HealthCheck(app, "/healthcheck")
 
-def chef_app_health():
+def app_health():
 	return True, "Health OK"
 
-health.add_check(chef_app_health)
+health.add_check(app_health)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
